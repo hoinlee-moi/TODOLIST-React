26524b05d8196221824979deb60039c5762482f3
hoinlee-moi <lhi3048@gmail.com>
2022-09-06 01:10:22 +0900

    add : todolist 틀 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoBoard from "./components/TodoBoard";
 
+// 1. 인풋창이 있고 버튼이 있다.
+// 2. 인풋창에 값을 입력하고 버튼을 클릭하면 아이템이 추가된다.
+// 3. 아이템 삭제버튼을 누르면 삭제가 가능
+// 4. 수정버튼을 누르면 수정이 가능
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const addItem = () => {
+    console.log("im herererer!", inputValue);
+    setTodoList([...todoList, inputValue]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <input value={inputValue} type="text" onChange={(event) => setInputValue(event.target.value)}></input>
+      <button onClick={addItem}>Add</button>
+
+      <TodoBoard />
+    </main>
   );
 }
 
